fe5ec693f1da852b9c7e9b3fd875ce25f8341737
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-11-20 01:35:13 -0800

    tests complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,30 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enhancement === 20) {
+    return { ...item };
+  } else {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+    return item;
+  } else if (item.enhancement >= 15 && item.enhancement <= 16) {
+    item.durability = item.durability - 10;
+    return item;
+  } else if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1;
+    return item;
+  } else return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
+
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,69 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer module", () => {
+  describe("repair function", () => {
+    it("exists", () => {
+      expect(enhancer.repair).toBeDefined();
+    });
+    it("returns an object", () => {
+      expect(enhancer.repair).toBeInstanceOf(Object);
+    });
+    it("restores durability to 100", () => {
+      expect(
+        enhancer.repair({ name: "Sword", durability: 80, enhancement: 20 })
+          .durability
+      ).toBe(100);
+    });
+  });
+  describe("success function", () => {
+    it("exists", () => {
+      expect(enhancer.success).toBeDefined();
+    });
+    it("returns an object", () => {
+      expect(enhancer.success).toBeInstanceOf(Object);
+    });
+    it("enhancement increases by 1", () => {
+      expect(enhancer.success({ enhancement: 15 })).toMatchObject({
+        enhancement: 16,
+      });
+    });
+    it("enhancement level is not changed if equal to 20", () => {
+      expect(enhancer.success({ enhancement: 20 })).toMatchObject({
+        enhancement: 20,
+      });
+    });
+  });
+  describe("fail function", () => {
+    it("exists", () => {
+      expect(enhancer.fail).toBeDefined();
+    });
+    it("returns an object", () => {
+      expect(enhancer.fail).toBeInstanceOf(Object);
+    });
+    it("decreases durability by 5 if enhancement level less than 15", () => {
+      expect(enhancer.fail({ durability: 100, enhancement: 14 })).toMatchObject(
+        {
+          durability: 95,
+          enhancement: 14,
+        }
+      );
+    });
+    it("decreases durability by 10 if enhancement level equal to or greater than 15", () => {
+      expect(enhancer.fail({ durability: 100, enhancement: 15 })).toMatchObject(
+        {
+          durability: 90,
+          enhancement: 15,
+        }
+      );
+    });
+    it("decreases enhancement by 1 if enhancement level greater than 16", () => {
+      expect(enhancer.fail({ durability: 100, enhancement: 17 })).toMatchObject(
+        {
+          durability: 100,
+          enhancement: 16,
+        }
+      );
+    });
+  });
+});
